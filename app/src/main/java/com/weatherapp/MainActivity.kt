bbfb49012d7c54efe9bbbb68fcd204152045db90
Joaquin <joaquin.lamela00@gmail.com>
2021-07-30 15:14:18 -0300

    Adding permission request for ubication

--- app/src/main/java/com/weatherapp/MainActivity.kt
+++ app/src/main/java/com/weatherapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.weatherapp
 
+import android.Manifest
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
@@ -8,17 +9,18 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.vmadalin.easypermissions.EasyPermissions
+import com.vmadalin.easypermissions.dialogs.SettingsDialog
 import com.weatherapp.databinding.ActivityMainBinding
 import com.weatherapp.repository.implementation.WeatherRepository
 import com.weatherapp.ui.home.HomeViewModel
 import com.weatherapp.ui.home.HomeViewModelFactory
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var homeVM: HomeViewModel
-
-
+    private val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +32,57 @@ class MainActivity : AppCompatActivity() {
         homeVM =
             ViewModelProvider(this, homeViewModelFactory).get(HomeViewModel::class.java)
 
+
+        if(hasLocationPermission()){
+            goToHome()
+        }else{
+            requestLocationPermission()
+        }
+
+    }
+
+    private fun hasLocationPermission() = EasyPermissions.hasPermissions(
+        this,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
+
+    private fun requestLocationPermission() {
+        EasyPermissions.requestPermissions(
+            this,
+            "This app need to get your location",
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: List<String>) {
+        when (requestCode) {
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
+                if (perms.isNotEmpty()) {
+                    goToHome()
+                }
+            }
+        }
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: List<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            SettingsDialog.Builder(this).build().show()
+        } else {
+            requestLocationPermission()
+        }
+    }
+
+    private fun goToHome(){
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -43,7 +96,6 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-
-
     }
+
 }
