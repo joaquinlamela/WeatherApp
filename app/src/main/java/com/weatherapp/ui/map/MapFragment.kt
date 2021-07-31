80f6ddc84cb9a71d768fea74ee9ed674056effc9
Joaquin <joaquin.lamela00@gmail.com>
2021-07-31 12:39:31 -0300

    Introducing the map fragment

--- app/src/main/java/com/weatherapp/ui/map/MapFragment.kt
+++ app/src/main/java/com/weatherapp/ui/map/MapFragment.kt
@@ -1,44 +1,58 @@
 package com.weatherapp.ui.map
 
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.weatherapp.R
 import com.weatherapp.databinding.FragmentMapBinding
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
 
-    private lateinit var mapViewModel: MapViewModel
     private var _binding: FragmentMapBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private var map: GoogleMap? = null
+    private var lastKnownLocation: Location? = null
+    private val DEFAULT_ZOOM = 13
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private val defaultLocation = LatLng(-34.905895, -56.164993)
+    private var longitude: Float = -56.164993f
+    private var latitude: Float = -34.905895f
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        mapViewModel =
-            ViewModelProvider(this).get(MapViewModel::class.java)
-
         _binding = FragmentMapBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(requireContext())
+        val mMapFragment = SupportMapFragment.newInstance()
+        childFragmentManager.beginTransaction().add(R.id.map, mMapFragment).commit()
+        mMapFragment.getMapAsync(this)
 
-        val textView: TextView = binding.textNotifications
-        mapViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+
+
+
+
+        return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+        TODO("Not yet implemented")
     }
 }
